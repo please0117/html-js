83034613fba3189fe1fc9b1428b347f457f8951e
please0117 <67898835+please0117@users.noreply.github.com>
2022-06-13 12:48:41 +0900

    Add files via upload

---
+++ number/num.js
@@ -0,0 +1,74 @@
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+var guesses = document.querySelector(".guesses");
+var lastResult = document.querySelector(".lastResult");
+var lowOrHi = document.querySelector(".lowOrHi");
+
+var guessSubmit = document.querySelector(".guessSubmit");
+var guessField = document.querySelector(".guessField");
+
+var guessCount = 1;
+var resetButton;
+
+function CheckGuess(){
+    var userGuess = Number(guessField.value);
+    if(guessCount === 1){
+        guesses.textContent = "Previous guesses: ";
+    }
+    guesses.textContent += userGuess + " ";
+
+    if(userGuess === randomNumber){
+        lastResult.textContent = "Congratulations! You got it right!";
+        lastResult.style.backgroundColor = "green";
+        lowOrHi.textContent = " ";
+        setGameover();
+    }
+    else if(guessCount === 10){
+        lastResult.textContent = "GameOver!";
+        lastResult.style.backgroundColor = "grey";
+        setGameover();
+    }
+    else{
+        lastResult.textContent = "Wrong!"; 
+        lastResult.style.backgroundColor = "red";
+        if(userGuess < randomNumber){
+            lowOrHi.textContent = "Last Guess was too low";
+        }
+        else if(userGuess > randomNumber){
+            lowOrHi.textContent = "Last Guess was too high";
+        }
+    }
+    guessCount++;
+    guessField.value = " ";
+    guessField.focus();
+}
+guessSubmit.addEventListener('click', CheckGuess);
+
+function setGameover(){
+    guessField.disabled = "true";
+    guessSubmit.disabled = "true";
+    resetButton = document.createElement("button");
+    resetButton.textContent = "start a new game";
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener("click", resetGame);
+}
+
+function resetGame(){
+    guessCount = 1;
+
+    var resetParas = document.querySelectorAll(".resultParas p");
+    for(var i = 0; i < resetParas.length; i++){
+        resetParas[i].textContent = " ";
+    }
+
+    resetButton.parentNode.removeChild(resetButton);
+
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = " ";
+    guessField.focus();
+
+    lastResult.style.backgroundColor = "White";
+
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+}
